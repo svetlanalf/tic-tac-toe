18ff43b77fdfffad6be6babe75748a18983809b4
svetlanalf <svetlanalf.by@gmail.com>
2021-08-31 01:59:57 +0300

    fix: getWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -11,11 +11,16 @@ class TicTacToe {
     }
 
     nextTurn(rowIndex, columnIndex) {
-        if (this.gameField[rowIndex][columnIndex] !== null) {
+        if (this.gameField[rowIndex][columnIndex] != null) {
             return;
         } 
         this.gameField[rowIndex][columnIndex] = this.currentPlayerSymbol;
-        this.currentPlayerSymbol = this.currentPlayerSymbol == 'x' ? 'o' : 'x';
+        if (this.currentPlayerSymbol === 'o') {
+            this.currentPlayerSymbol = 'x';
+        } else {
+            this.currentPlayerSymbol = 'o';
+        }
+        //this.currentPlayerSymbol = this.currentPlayerSymbol === 'x' ? 'o' : 'x';
     }
 
     isFinished() {
@@ -26,34 +31,20 @@ class TicTacToe {
         
         let player = null;
         for (let x = 0; x < this.gameField.length; x++) {
-            player = this.gameField[x][0];
-            if (player === null) {
-                break;
-            }
-            for (let y = 1; y < this.gameField[x].length; y++) {
-                if (player !== this.gameField[x][y]) {
-                    break;
-                }
-                if (y === this.gameField[x].length - 1) {
-                    return player;
-                }
-            }
+            if (this.gameField[x][0] === this.gameField[x][1] && this.gameField[x][1] === this.gameField[x][2]) {
+                return this.gameField[x][2];
+            }            
         }
         for (let y = 0; y < this.gameField[0].length; y++) {
-            player = this.gameField[0][y];
-            if (player === null) {
-                break;
-            }
-            for (let x = 1; x < this.gameField.length; x++) {
-                if (player !== this.gameField[x][y]) {
-                    break;
-                }
-                if (x === this.gameField.length - 1) {
-                    return player;
-                }
+            if (this.gameField[0][y] === this.gameField[1][y] && this.gameField[1][y] === this.gameField[2][y]) {
+                return this.gameField[0][y];
             }
         }
 
+        if (this.gameField[0][0] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[2][2]) {
+            return this.gameField[2][2];
+        }
+        /*
         player = this.gameField[0][0];
         if (player !== null) {
             for (let x = 1; x < this.gameField.length; x++) {
@@ -67,7 +58,12 @@ class TicTacToe {
                 }
             }
         }
+*/
 
+        if (this.gameField[0][2] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[2][0]) {
+            return this.gameField[0][2];
+        }
+        /*
         player = this.gameField[0][this.gameField.length - 1];
         if (player !== null) {
             for (let x = 1; x < this.gameField.length; x++) {
@@ -80,7 +76,7 @@ class TicTacToe {
                     }
                 }
             }
-        }
+        }*/
 
         return null;
 
